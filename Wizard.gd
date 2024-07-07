091ab0b00312f55bd1e62d4b75fe153b7983095c
capgames <allo.client345@gmail.com>
2024-07-07 22:22:31 +0300

    wizard character

--- Wizard.gd
+++ Wizard.gd
@@ -1,1 +1,7 @@
 extends CharacterBody2D
+var SPEED = 300
+func _process(delta):
+	velocity.x = (Input.get_action_strength("right")-Input.get_action_strength("left"))*SPEED
+	velocity.y = (Input.get_action_strength("down")-Input.get_action_strength("up"))*SPEED
+	velocity.normalized()
+	move_and_slide()
