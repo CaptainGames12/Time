6df1008896aadd828c58964429f0f733f32bcecb
capgames <allo.client345@gmail.com>
2024-08-26 18:51:36 +0300

    assets and shop

--- Wizard.gd
+++ Wizard.gd
@@ -1,17 +1,32 @@
 extends CharacterBody2D
 var SPEED = 300
+@onready var fireball = preload("res://fireball.tscn")
+var window
+
+	
+	
 func _process(delta):
 	velocity.x = (Input.get_action_strength("right")-Input.get_action_strength("left"))*SPEED
 	velocity.y = (Input.get_action_strength("down")-Input.get_action_strength("up"))*SPEED
 	velocity.normalized()
-	var mouse_pos = get_global_mouse_position()
-	var mouse_button_pressed = Input.is_action_just_pressed("mouse")
-	match mouse_button_pressed:
-		1:
-			attack(mouse_pos)
+	window = get_viewport_rect().size
+	if Input.is_action_just_pressed("mouse"):
+		attack()
+	if Input.is_action_just_pressed("time_stop"):
+		get_tree().paused = !get_tree().paused
 	
+		
 	move_and_slide()
-func attack(mouse):
-	get_tree().add_child("res://fireball.tscn")
-	$Fireball.position = position
-	$Fireball.position+=mouse
+func attack():
+	
+	var bullet = fireball.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = position
+	bullet.target_fire = (position-get_global_mouse_position()).normalized()
+		
+		
+	
+	
+
+
+
