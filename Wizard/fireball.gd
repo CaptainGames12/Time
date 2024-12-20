21ad833a720a343374f14017fc06432868888e8d
CaptainGames12 <allo.client345@gmail.com>
2024-12-20 16:46:04 +0200

    money

--- Wizard/fireball.gd
+++ Wizard/fireball.gd
@@ -1,6 +1,7 @@
 extends Area2D
 @export var speed = 200
 var target_fire: Vector2
+var COIN = preload("res://coin.tscn").instantiate()
 func _physics_process(delta):
 	position-=target_fire*speed*delta
 	
@@ -11,5 +12,9 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_body_entered(body):
+	COIN.position = body.position
+	
 	body.queue_free()
 	queue_free()
+	get_parent().call_deferred("add_child", COIN)
+	
